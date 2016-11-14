81cc03288a387c7e0292c9f98b32963188fea6d4
Guillermo <guillermo.fernandez@acneuro.com>
2016-11-14 15:02:09 +0100

    Adding Junit

--- src/test/java/com/oguiller/beer/BeerTest.java
+++ src/test/java/com/oguiller/beer/BeerTest.java
@@ -1,4 +1,17 @@
 package com.oguiller.beer;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class BeerTest {
+
+    @Test
+    public void heinekenIsCrap(){
+        Assert.assertEquals(true, true);
+    }
+
+    @Test
+    public void dutchPainIsAwesome(){
+        Assert.assertEquals(true, true);
+    }
 }
